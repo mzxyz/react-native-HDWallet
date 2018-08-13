ca68ddc2992535365b4d0efed4fa3250dba9cf57
Michael <michaelzhaihk@gmail.com>
2018-08-14 11:45:20 +1200

    Add js apis

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 
 import { NativeModules } from 'react-native';
+import { Buffer } from 'buffer';
 
 const { RNHDWallet } = NativeModules;
 
-export default RNHDWallet;
+export const generateMnemonic = () => {
+  return RNHDWallet.generateMnemonic();
+};
+
+export const mnemonicFromSeed = (seed) => {
+  if (Buffer.isBuffer(seed)) {
+    return RNHDWallet.mnemonicFromSeed(seed);
+  }
+
+  if (buffer instanceof Uint8Array) {
+    return RNHDWallet.mnemonicFromSeed(Buffer.from(seed.buffer));
+  }
+
+  throw new TypeError('unexpected type, use Buffer or Uint8Array');
+};
+
+export const seedFromMnemonic = (mnemonic) => {
+  return RNHDWallet.seedFromMnemonic(mnemonic);
+};
+
+export const validateMnemonic = mnemonic => {
+  return RNHDWallet.validateMnemonic(mnemonic);
+};
